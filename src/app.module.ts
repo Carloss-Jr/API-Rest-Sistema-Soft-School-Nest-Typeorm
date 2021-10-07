e2bfbdccb43a18fa12edcf2c6a3d3393921f1df9
Carloss-Jr <carlos.gluckk@gmail.com>
2021-10-07 01:14:30 -0300

    delete app.controler/test/service + connect bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'adminSS',
+      database: 'SoftSchool',
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
