45a869b3caad149dddb02213db86ab63e21a2f8b
Carloss-Jr <carlos.gluckk@gmail.com>
2021-10-07 01:28:37 -0300

    yarn add @nest/config + .env TYPEORMCONFIGS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'adminSS',
-      database: 'SoftSchool',
+      type: process.env.TYPE_ORM_CONNECTION,
+      host: process.env.TYPE_ORM_HOST,
+      port: process.env.TYPE_ORM_PORT,
+      username: process.env.TYPE_ORM_USERNAME,
+      password: process.env.TYPE_ORM_PASSWORD,
+      database: process.env.TYPE_ORM_DATABASE,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
 
-    })
+    } as TypeOrmModuleOptions)
   ],
   controllers: [],
   providers: [],
