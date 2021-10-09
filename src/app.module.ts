a4ee59384871905dc983e75e26b0e39a46db5bc2
Carloss-Jr <carlos.gluckk@gmail.com>
2021-10-09 01:09:01 -0300

    teste prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 import { UsersController } from './app/controller/users.controller';
 import { UsersModule } from './app/models/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.TYPE_ORM_CONNECTION,
-      host: process.env.TYPE_ORM_HOST,
-      port: process.env.TYPE_ORM_PORT,
-      username: process.env.TYPE_ORM_USERNAME,
-      password: process.env.TYPE_ORM_PASSWORD,
-      database: process.env.TYPE_ORM_DATABASE,
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      synchronize: true,
-    } as TypeOrmModuleOptions),
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: async () =>
+      Object.assign(await getConnectionOptions(), {
+        autoLoadEntities: true
+      })
+  }),
     UsersModule,
     AuthModule,
   ],
@@ -26,3 +20,15 @@ import { AuthModule } from './auth/auth.module';
   providers: [],
 })
 export class AppModule { }
+
+// ConfigModule.forRoot(),
+// TypeOrmModule.forRoot({
+//   type: process.env.TYPE_ORM_CONNECTION,
+//   host: process.env.TYPE_ORM_HOST,
+//   port: process.env.TYPE_ORM_PORT,
+//   username: process.env.TYPE_ORM_USERNAME,
+//   password: process.env.TYPE_ORM_PASSWORD,
+//   database: process.env.TYPE_ORM_DATABASE,
+//   entities: [__dirname + '/**/*.entity{.js,.ts}'],
+//   synchronize: true,
+// } as TypeOrmModuleOptions),
