690674ca82c067fdf007e38d6a398edbbc94b265
Carloss-Jr <carlos.gluckk@gmail.com>
2021-10-09 01:55:21 -0300

    revert erro

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,40 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnectionOptions } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { UsersController } from './app/controller/users.controller';
 import { UsersModule } from './app/models/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({
-    useFactory: async () =>
-      Object.assign(await getConnectionOptions(), {
-        autoLoadEntities: true
-      })
-  }),
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPE_ORM_CONNECTION,
+      host: process.env.TYPE_ORM_HOST,
+      port: process.env.TYPE_ORM_PORT,
+      username: process.env.TYPE_ORM_USERNAME,
+      password: process.env.TYPE_ORM_PASSWORD,
+      database: process.env.TYPE_ORM_DATABASE,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
     UsersModule,
     AuthModule,
   ],
-  controllers: [UsersController],
-  providers: [],
+    controllers: [UsersController],
+    providers: [],
 })
 export class AppModule { }
+
+// [TypeOrmModule.forRootAsync({
+//   useFactory: async () =>
+//     Object.assign(await getConnectionOptions(), {
+//       autoLoadEntities: true
+//     })
+// }),
+ 
 
 // ConfigModule.forRoot(),
 // TypeOrmModule.forRoot({
